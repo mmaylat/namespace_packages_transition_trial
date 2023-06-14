5f15a990698e846564ca8f71a3e2cbc9932c9a19
Maylat, Maoz <maoz.maylat@intel.com>
2023-06-14 23:33:08 +0300

    Transition root-namespace to implicit namespace definition

--- root_namespace_proj_ref/proj_ref/__init__.py
+++
@@ -1,6 +0,0 @@
-# try to use declare namespace, but if that fails, use pkgutil
-try:
-    __import__('pkg_resources').declare_namespace(__name__)
-except ImportError:     # pragma: no cover
-    import pkgutil
-    pkgutil.extend_path(__path__, __name__)
